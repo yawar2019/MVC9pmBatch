615a6ee308e4f56a62b999c7add4bbba67a814f3
yawar2019 <yawarali17@gmail.com>
2020-12-19 21:43:03 +0530

    comitted code

--- CodeFirstApproach/Controllers/DefaultController.cs
+++ CodeFirstApproach/Controllers/DefaultController.cs
@@ -14,5 +14,67 @@ namespace CodeFirstApproach.Controllers
         {
             return View(db.EmployeeModeles.ToList());
         }
+
+        public ActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            db.EmployeeModeles.Add(emp);
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModeles.Find(id);
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel emp)
+        {
+            db.Entry(emp).State=System.Data.Entity.EntityState.Modified;
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult Delete(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModeles.Find(id);
+            return View(emp);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModeles.Find(id);
+            db.EmployeeModeles.Remove(emp);
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
     }
 }
