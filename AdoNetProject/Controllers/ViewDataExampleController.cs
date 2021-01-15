db852cd12489073ee022fa3c24ed6740e34da81f
yawar2019 <yawarali17@gmail.com>
2021-01-15 21:56:58 +0530

    comitted code

--- AdoNetProject/Controllers/ViewDataExampleController.cs
+++ AdoNetProject/Controllers/ViewDataExampleController.cs
@@ -11,14 +11,21 @@ namespace AdoNetProject.Controllers
         // GET: ViewDataExample
         public ActionResult Index()
         {
-            ViewData["id"] = 1211;
-            ViewBag.info2 = "hi";
+            //ViewData["id"] = 1211;
+            //ViewBag.info2 = "hi";
+            TempData["tinfo"] = "prachi";
+
             return RedirectToAction("Index1");
         }
         public ActionResult Index1()
         {
-           string info= ViewData["id"].ToString();
-            string s = ViewBag.info2;
+            //string k = TempData["tinfo"].ToString();
+            //string info= ViewData["id"].ToString();
+            //string s = ViewBag.info2;
+            //TempData.Keep();
+            string k = TempData.Peek("tinfo").ToString();
+
+            ViewBag.infotemp = k;
             return View();
         }
     }
